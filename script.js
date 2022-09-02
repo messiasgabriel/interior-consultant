c71858f2a9592f7762b3f9c9bd888244ee669429
Gabriel Messias <72223185+messiasgabriel@users.noreply.github.com>
2022-09-02 14:17:01 -0300

    Menu test

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const btnMobile = document.getElementById('btn');
 
 function toggleMenu (){
      const nav = document.getElementById('nav');
-     nav.classList.toggle('active');
-    
+     nav.classList.toggle('active'); 
 }
 btnMobile.addEventListener('click', toggleMenu);
